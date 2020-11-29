db856c29032601a2d5642ca89728b8c51e2a3d4a
Yurii <airtopsales@gmail.com>
2020-11-30 00:48:40 +0300

    1commit

--- lesson_7/lesson_7-1.py
+++ lesson_7/lesson_7-1.py
@@ -1,11 +1,8 @@
-# Написать функцию которая вернет строку введенную пользователем.
-# Обернуть функцию в декоратор чтобы функция вместо строки целиком вернула список слов.
-
-def to_list(any_str):
-    return any_str.split(' ')
-
+def to_list(func):
+    print(func().split(' '))
+@to_list
 def user_input():
     user_str = str(input('Введите строку: '))
     return user_str
 
-print(to_list(user_input()))
+
