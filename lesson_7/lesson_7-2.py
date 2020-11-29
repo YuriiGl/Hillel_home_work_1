22322a1cd3f67c1ed535a1e9ab2fc4ae4281c270
Yurii <airtopsales@gmail.com>
2020-11-29 23:38:01 +0300

    1commit

---
+++ lesson_7/lesson_7-2.py
@@ -0,0 +1,13 @@
+any_temp = float(input('Введите температуру: '))
+any_sist = str(input('Введите систему измерения температуры (C, K, F):'))
+if any_sist == 'C':
+    print(str(any_temp),'C   ', str(any_temp + 273.15),'K   ', str(any_temp * 1.8 + 32), 'F   ' )
+elif any_sist == 'K':
+    print(str(any_temp - 273.15), 'C   ', str(any_temp), 'K   ', str(any_temp * 1.8 - 459.67) + 'F   ')
+elif any_sist == 'F':
+    print(str((any_temp - 32)*1.8) + 'C   ', str((any_temp + 459.67)/1.8) + 'K   ', str(any_temp) + 'F   ')
+else:
+    print('Неправильный ввод!')
+
+
+# Какая разница между запятой и "+" при конкатенации?
--- lesson_7/lesson_7-3.py
+++ lesson_7/lesson_7-3.py
@@ -1,6 +1,4 @@
-import urllib.request
 import requests
-import json
 URL='http://api.openweathermap.org/data/2.5/forecast/daily'
 def return_weather(days=1):
     data = {'q' : 'Odesa','cnt' : days, 'units':'metric','appid':'f9ada9efec6a3934dad5f30068fdcbb8'}
@@ -8,11 +6,12 @@ def return_weather(days=1):
     return r.json()
 result = return_weather(days = 3)
 def write_file():
-    prin
+    print()
 print('температура днем\tТемпература ночью\tПо ощущениям днем\tПо ощущениям ночью')
 #print(result)
 for day in result['list']:
-   print(day['temp']['day'],'\t', day['feels_like']['day'],'\t', day['temp']['night'],'\t', day['feels_like']['night'] )
+   print(day['temp']['day'],'\t', '\t', '\t',  '\t', '\t', day['feels_like']['day'],'\t' '\t', '\t', '\t', '\t',
+         day['temp']['night'],'\t' '\t', '\t', '\t', '\t', day['feels_like']['night'] )
 print('done')
 
 import datetime
