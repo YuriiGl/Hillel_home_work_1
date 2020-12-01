902fb5120e454b1a2278e1ed3febb626b21fd5cc
Yurii <airtopsales@gmail.com>
2020-12-01 22:42:11 +0300

    1commit

--- lesson_7/lesson_7-3.py
+++ lesson_7/lesson_7-3.py
@@ -2,15 +2,15 @@ import datetime
 import requests
 
 URL='http://api.openweathermap.org/data/2.5/forecast/daily'
+any_days = int(input('Введите колличество дней: '))
 def return_weather(days=1):
     data = {'q' : 'Odesa','cnt' : days, 'units':'metric','appid':'f9ada9efec6a3934dad5f30068fdcbb8'}
     r = requests.get(URL, data)
     return r.json()
-result = return_weather(days = 3)
+result = return_weather(any_days)
 def write_file():
     print()
 print('\tдата\t\t\tтемпература днем\tТемпература ночью\tПо ощущениям днем\tПо ощущениям ночью')
-#print(result)
 for day in result['list']:
     date = day['dt']
     print(datetime.datetime.fromtimestamp(date).strftime("%d-%m-%Y"), '\t', '\t', '\t', '\t', day['temp']['day'],'\t', '\t',  '\t', '\t', day['feels_like']['day'],'\t' '\t', '\t', '\t', '\t',
