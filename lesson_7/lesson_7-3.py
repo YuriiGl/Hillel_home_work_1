39ce0aa5900445337e1411b041471271e71ac079
Yurii <airtopsales@gmail.com>
2020-12-01 21:58:03 +0300

    1commit

--- lesson_7/lesson_7-3.py
+++ lesson_7/lesson_7-3.py
@@ -1,4 +1,6 @@
+import datetime
 import requests
+
 URL='http://api.openweathermap.org/data/2.5/forecast/daily'
 def return_weather(days=1):
     data = {'q' : 'Odesa','cnt' : days, 'units':'metric','appid':'f9ada9efec6a3934dad5f30068fdcbb8'}
@@ -7,12 +9,13 @@ def return_weather(days=1):
 result = return_weather(days = 3)
 def write_file():
     print()
-print('температура днем\tТемпература ночью\tПо ощущениям днем\tПо ощущениям ночью')
+print('\tдата\t\t\tтемпература днем\tТемпература ночью\tПо ощущениям днем\tПо ощущениям ночью')
 #print(result)
 for day in result['list']:
-   print(day['temp']['day'],'\t', '\t', '\t',  '\t', '\t', day['feels_like']['day'],'\t' '\t', '\t', '\t', '\t',
-         day['temp']['night'],'\t' '\t', '\t', '\t', '\t', day['feels_like']['night'] )
+    date = day['dt']
+    print(datetime.datetime.fromtimestamp(date).strftime("%d-%m-%Y"), '\t', '\t', '\t', '\t', day['temp']['day'],'\t', '\t',  '\t', '\t', day['feels_like']['day'],'\t' '\t', '\t', '\t', '\t',
+         day['temp']['night'],'\t' '\t', '\t', '\t', day['feels_like']['night'] )
+
 print('done')
 
-import datetime
 print(datetime.datetime.today())
