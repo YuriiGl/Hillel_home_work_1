82a4bc949c4668ec71e82e5d50664149648d9635
Yurii <airtopsales@gmail.com>
2020-12-08 15:44:14 +0300

    11-2

--- lesson_11/lesson_11-2.py
+++ lesson_11/lesson_11-2.py
@@ -1,5 +1,5 @@
-# 2. Написать функцию которая будет частично скрывать e-mail, пример:
-#   hide_email(somebody_email@gmail.com) -> em***@**il.com
-
-hide_email = str(input('Введите свой e-mail: '))
-def hide_email():
+def hide_email (email):
+    email = '@'.join([email.replace(email[email.find('@') - 3:], '***'),
+                      email.replace(email[:len(email) - email.find('@') + 2], '**')])
+    return email
+print(hide_email('somebody_email@gmail.com'))
