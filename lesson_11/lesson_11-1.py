1954f282023c1bbdd356e4f5f6cfd6c01ecfd82d
Yurii <airtopsales@gmail.com>
2020-12-10 14:02:31 +0300

    lesson_11-1.py (исправил)

--- lesson_11/lesson_11-1.py
+++ lesson_11/lesson_11-1.py
@@ -2,8 +2,9 @@ win = int(input("Win: "))
 draw = int(input("Draw: "))
 loss = int(input("loss: "))
 def count_points(win, draw, loss):
-    return(count_points())
-print('count_points (', win, draw, loss,') ->',  win * 3 + draw)
+    any_sum =  win * 3 + draw * 1 + loss * 04
+    return any_sum
+print('count_points (win, draw, loss) -> ', count_points(win, draw, loss))
 
 
 
