fd4840853aef64fef05eb36c70aec7054f3d6968
Yurii <airtopsales@gmail.com>
2021-01-08 00:33:42 +0300

    Готово

--- lesson_15/lesson_15-15.py
+++ lesson_15/lesson_15-15.py
@@ -1,71 +1,58 @@
-# # Во вложении файл csv с данными про аэропорты мира, написать программу которая сможет
-# # вернуть данные по таким параметрам:
-# # --iata_code - код аэропорта, должно вернуть 1 запись по коду аэропорта(всю строку) или вернуть ошибку AirportNotFoundError
-# # --country - страна, должно вернуть все записи по аэропортам или
-# # CountryNotFoundError
-# # --name - значение имени аэропорта, допустимо вхождение строки хотябы
-# # минимально, т.е. liman должен вернуть строки с такими названиями:
-# # Ilimanaq Heliport
-# # Sidi Slimane Airport
-# # Kilimanjaro International Airport
-# # West Kilimanjaro Airport
-# # Limanske Airfield
-# # Liman Airfield
-# # или AirportNotFoundError
-# # Только один параметр обязателен, если выбрано несколько - вернуть
-# # ошибку:
-# # MultipleOptionsError, если ни одного - NoOptionsFoundError
-# # ** доп. ошибки принимают два аргумента, текст ошибки и входные данные,
-# # пример:
-# # AirportNotFoundError: ('Airport not found', 'OESD')
-# # CountryNotFoundError: ('Country not found', 'UGUGU')
-#
-# import csv
-#
-# # filename = 'airport-codes_csv.csv'
-# # with open(filename) as f:
-# #     reader = csv.reader(f)
-# #     header_row = next(reader)
-# # print(header_row)
-# class AirportFounder:
-#     def __init__(self, arguments):
-#         self.args = self.check_arguments(arguments)
-#     def check_arguments(self, arguments):
-#         my_args = ['iata_code', 'country', 'name']
-#         if arguments in my_args:
-#             return arguments
-#         # доделать валидацию
-#     def find(self):
-#         filename = 'airport-codes_csv.csv'
-#         with open(filename, encoding='utf-8') as f:
-#             reader = csv.reader(f)
-#             header_row = next(reader)
-#             for index, column_header in enumerate(header_row):
-#                 print(index, column_header)
-#             iata_code_list,contry_list, name_list  = [], [], []
-#             for row in reader:
-#                 iata_code = str(row[9])
-#                 contry = str(row[5])
-#                 name = str(row[2])
-#                 iata_code_list.append(iata_code)
-#                 contry_list.append(contry)
-#                 name_list.append(name)
-#
-# a1 = AirportFounder('name')
-# a1.find()
+# Во вложении файл csv с данными про аэропорты мира, написать программу которая сможет
+# вернуть данные по таким параметрам:
+# --iata_code - код аэропорта, должно вернуть 1 запись по коду аэропорта(всю строку) или вернуть ошибку AirportNotFoundError
+# --country - страна, должно вернуть все записи по аэропортам или
+# CountryNotFoundError
+# --name - значение имени аэропорта, допустимо вхождение строки хотябы
+# минимально, т.е. liman должен вернуть строки с такими названиями:
+# Ilimanaq Heliport
+# Sidi Slimane Airport
+# Kilimanjaro International Airport
+# West Kilimanjaro Airport
+# Limanske Airfield
+# Liman Airfield
+# или AirportNotFoundError
+# Только один параметр обязателен, если выбрано несколько - вернуть
+# ошибку:
+# MultipleOptionsError, если ни одного - NoOptionsFoundError
+# ** доп. ошибки принимают два аргумента, текст ошибки и входные данные,
+# пример:
+# AirportNotFoundError: ('Airport not found', 'OESD')
+# CountryNotFoundError: ('Country not found', 'UGUGU')
+# IATA код может быть только 3х буквенным в верхнем регистре, сделать валидацию на него
+# или вернуть IATACodeError
 
-
+import argparse
 import csv
 import re
+
+class AirportNotFoundError(Exception):
+    pass
+
+class MultipleOptionsError(Exception):
+    pass
+
+class NoOptionsFoundError(Exception):
+    pass
+
 class AirportFounder:
-    def __init__(self, arguments, value):
+    def __init__(self, arguments):
         self.args = self.check_arguments(arguments)
-        self.value = value
+
     def check_arguments(self, arguments):
-        my_args = ['iata_code', 'country', 'name']
-        if arguments in my_args:
-            return arguments
-        # доделать валидацию
+        args = [('name', arguments.name), ('country', arguments.country), ('iata_code', arguments.iata_code)]
+        res = []
+        for a in args:
+            if a[1] is not None:
+                res.append(a)
+
+        if len(res) > 1:
+            raise MultipleOptionsError
+        elif len(res) == 0:
+            raise NoOptionsFoundError
+        else:
+            return res
+
     def find(self):
         filename = 'airport-codes_csv.csv'
         with open(filename, encoding='utf-8') as f:
@@ -73,19 +60,24 @@ class AirportFounder:
             header_row = next(reader)
             target_column = -1
             for index, column_header in enumerate(header_row):
-                if(column_header==self.args):
+                if(column_header==self.args[0][0]):
                     target_column = index
-             #if(target_column==-1):
-              #raise Error #кидаем ошибку что нет такой колонки
-             #print('target index: ',target_column)
-                #print(index, column_header)
             result = []
+            pattern = rf"[\w\s]*{self.args[0][1]}[\w\s]*"
 
             for row in reader:
-                if(row[target_column] == self.value):
+                if (re.fullmatch(pattern, row[target_column], flags=re.IGNORECASE)):
                     result.append(row)
+        if len(result) == 0:
+            raise AirportNotFoundError
+
         for r in result:
             print(r)
-#Lowell Field
-a1 = AirportFounder('name','Lowell Field')
+
+parser = argparse.ArgumentParser(description='World Airports')
+parser.add_argument('--iata_code')
+parser.add_argument('--country')
+parser.add_argument('--name')
+arguments =  parser.parse_args()
+a1 = AirportFounder(arguments)
 a1.find()
