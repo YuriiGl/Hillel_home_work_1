ae7812c5db1f40eec3276095e27167a400d26dc0
Yurii <airtopsales@gmail.com>
2021-01-17 20:23:30 +0300

    В связи с тем, что проверять можно только функции, пришлось в корне переделать задание 7-2. +10 балов к карме ))
    Signed-off-by: Yurii <airtopsales@gmail.com>

--- lesson_7/lesson_7-2.py
+++ lesson_7/lesson_7-2.py
@@ -10,3 +10,25 @@ else:
     print('Неправильный ввод!')
 
 # Какая разница между запятой и "+" при конкатенации?
+
+# В связи с тем, что проверять можно только функции, єто задание пришлось переделать с нуля. Представляю еще один вариант.
+
+def temperature(sist, temp):
+	if sist == 'C':
+		return (temp, temp + 273.15, temp * 1.8 + 32)
+	elif any_sist == 'K':
+		return (temp - 273.15, temp, temp * 1.8 - 459.67)
+	elif any_sist == 'F':
+		return ((temp - 32)*1.8, (temp + 459.67)/1.8, temp)
+	else:
+		print('Неправильный ввод!')
+
+def main():
+	any_temp = float(input('Введите температуру: '))
+	any_sist = str(input('Введите систему измерения температуры (C, K, F): '))
+
+	(c, k, f) = temperature(any_sist, any_temp)
+	print(str(c),'C   ', str(k),'K   ', str(f), 'F   ' )
+
+if '__name__'=='__main__':
+	main()
